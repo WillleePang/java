60d7bc3b8205bce48ecd70fb8ca184da13adbb89
willlee <pangwilllee@hotmail.com>
2016-03-09 16:26:07 +0800

    删除了.idea文件夹

--- LearnJava/src/com/willlee/jdbc/integertest.java
+++ LearnJava/src/com/willlee/jdbc/integertest.java
@@ -2,11 +2,20 @@ package com.willlee.jdbc;
 
 public class integertest {
 	public static void main(String[] args) {
-		int  i = 128;
+		int i = 128;
 		int i1 = new Integer(128);
 		System.out.println(i == i1);
 		int i2 = 205;
-		int i3  = new Integer(205);
+		int i3 = new Integer(205);
 		System.out.println(i2 == i3);
+
+		int I = 5, J = 5;
+		System.out.println((I++) + (I++) + (I++));
+		System.out.println((++J) + (++J) + (++J));
+	}
+
+	public int getInt() {
+		char a = '1';
+		return a;
 	}
 }
