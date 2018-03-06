4897851d9a69add7b04e5b597a1f68a53af10ca6
willleepang <pangwilllee@hotmail.com>
2018-03-06 20:06:29 +0800

    添加了一个netty的聊天实例
    如题

---
+++ Practice/src/com/willlee/netty/timeout/HeartbeatHandlerInitializer.java
@@ -0,0 +1,21 @@
+package com.willlee.netty.timeout;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.timeout.IdleStateHandler;
+
+public class HeartbeatHandlerInitializer extends ChannelInitializer<Channel> {
+
+	private static final int READ_IDEL_TIME_OUT = 4;
+	private static final int WRITE_IDEL_TIME_OUT = 5;
+	private static final int ALL_IDEL_TIME_OUT = 7;
+
+	@Override
+	protected void initChannel(Channel ch) throws Exception {
+		ChannelPipeline pipeline = ch.pipeline();
+		pipeline.addLast(new IdleStateHandler(READ_IDEL_TIME_OUT, WRITE_IDEL_TIME_OUT, ALL_IDEL_TIME_OUT));
+		pipeline.addLast(new HeartbeatServerHandler());
+	}
+
+}
