7c624dc06f0b54530a62a5fd7efe808cce766436
willleepang <pangwilllee@hotmail.com>
2019-04-17 11:54:10 +0800

    增加了nio的java类

---
+++ Advance/src/main/java/com/willlee/nio/ChannelExample.java
@@ -0,0 +1,26 @@
+package com.willlee.nio;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+
+public class ChannelExample {
+    public static void main(String[] args) {
+        ReadableByteChannel readableByteChannel = Channels.newChannel(System.in);
+        WritableByteChannel writableByteChannel = Channels.newChannel(System.out);
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        try {
+            while (readableByteChannel.read(buffer) != -1) {
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    writableByteChannel.write(buffer);
+                }
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
