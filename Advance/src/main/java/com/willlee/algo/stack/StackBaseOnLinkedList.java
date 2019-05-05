ba7ef2a51fc9b15253ea57232ad106d83c00484c
willleepang <pangwilllee@hotmail.com>
2019-05-05 16:21:16 +0800

    增加了stack有关的数据结构和基本算法

---
+++ Advance/src/main/java/com/willlee/algo/stack/StackBaseOnLinkedList.java
@@ -0,0 +1,45 @@
+package com.willlee.algo.stack;
+
+public class StackBaseOnLinkedList {
+    private Node top = null;
+
+    public void push(int value) {
+        Node newNode = new Node(value, null);
+        if (top == null) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+    }
+
+    public int pop() {
+        if (top == null) {
+            return -1;
+        } else {
+            int value = top.data;
+            top = top.next;
+            return value;
+        }
+    }
+
+    public void printAll() {
+        Node p = top;
+        while (p != null) {
+            System.out.print(p.data + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    private static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+    }
+}
