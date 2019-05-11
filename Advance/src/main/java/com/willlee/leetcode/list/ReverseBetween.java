fdad2bf90939be3758b7311ecc542fb62144ef0a
willlee <pangwilllee@hotmail.com>
2019-05-11 19:02:58 +0800

    增加了几个leetcode的solution

---
+++ Advance/src/main/java/com/willlee/leetcode/list/ReverseBetween.java
@@ -0,0 +1,39 @@
+package com.willlee.leetcode.list;
+
+//leetcode92
+public class ReverseBetween {
+	public static ListNode reverseBetween(ListNode head, int m, int n) {
+		ListNode head1 = new ListNode(-1);
+		head1.next = head;
+		ListNode pre = head1;
+		int i = 1;
+		while (head != null) {
+			if (i >= m && i < n) {
+				ListNode temp = head.next;
+				head.next = temp.next;
+				temp.next = pre.next;
+				pre.next = temp;
+			} else {
+				pre = head;
+				head = head.next;
+			}
+			i++;
+		}
+		return head1.next;
+	}
+
+	public static void main(String[] args) {
+		ListNode a1 = new ListNode(1);
+		ListNode a2 = new ListNode(2);
+		ListNode a3 = new ListNode(3);
+		ListNode a4 = new ListNode(4);
+		ListNode a5 = new ListNode(5);
+		a1.next = a2;
+		a2.next = a3;
+		a3.next = a4;
+		a4.next = a5;
+		a1.printList();
+		a1 = reverseBetween(a1, 1, 5);
+		a1.printList();
+	}
+}
