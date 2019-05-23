cbd2bdd3c1dd9bb5fb4c160c44dc1e51bdf92d2d
willleepang <pangwilllee@hotmail.com>
2019-05-23 11:51:33 +0800

    增加了leetcode的solution

--- Advance/src/main/java/com/willlee/leetcode/array/CombinationSum.java
+++ Advance/src/main/java/com/willlee/leetcode/array/CombinationSum.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//leetcode39
+//leetcode39 40
 public class CombinationSum {
     // 做该题的重要条件是无重复的数组，那么问题就很好解了。首先对数组从大到小排序。这是解题的关键。
     // 1、target循环减去一个数，如果能一直减到到差值等于0，那么这个数组成的数组就是一个解,比如[2,2,2,2];
@@ -39,7 +39,48 @@ public class CombinationSum {
                 }
             } // end for
         }
-
     }
 
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        backTrack(candidates, target, 0, res, new ArrayList<>());
+        return res;
+    }
+
+    private void backTrack(int[] candidates, int target, int start, List<List<Integer>> res, List<Integer> temp) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            res.add(temp);
+            return;
+        }
+        /*
+         * 最开始我是这样去重的 但是发现会一起把重复元素组成的解去掉。比如： 输入 : [10,1,2,7,6,1,5] 8 输出 :
+         * [[1,2,5],[1,7],[2,6]] 缺少了 [1,1,6] 这个解
+         */
+        // if (start != 0 && candidates[start] == candidates[start - 1]) {
+        // return;
+        // }
+        /*
+         * 从start到candidate末尾的元素来搜索有没有组成target的可能性 target >=
+         * candidates[i]意思是，如果当前的元素已经大于target就不要往下搜索了
+         */
+        for (int i = start; i < candidates.length && target >= candidates[i]; i++) {
+            /*
+             * 这个是看了别人的去重方法，巧妙的利用了start位置上的元素不用搜索前面有没有重复的
+             * 这一个特性。它把重复元素（[1,1,6]）的搜索任务都交给了第一个出现的元素去搜索，当 第二次出现时，就continue跳过了。
+             * 另外像（[1,2,5],[1,7]）其中的1也是第一个1而不是第二个1。
+             */
+            if (i != start && candidates[i] == candidates[i - 1]) {
+                continue;
+            } else {
+                List<Integer> newTemp = new ArrayList<>(temp);
+                newTemp.add(candidates[i]);
+                // 这里的i + 1也不能写成++ i
+                backTrack(candidates, target - candidates[i], i + 1, res, newTemp);
+            }
+        }
+    }
 }
