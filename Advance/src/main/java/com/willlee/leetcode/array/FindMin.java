77adb964ee643ae33f4dc5af650706d5ee7ab9bc
willleepang <pangwilllee@hotmail.com>
2019-07-19 15:19:29 +0800

    add solution

---
+++ Advance/src/main/java/com/willlee/leetcode/array/FindMin.java
@@ -0,0 +1,29 @@
+package com.willlee.leetcode.array;
+
+//leetcode153 154
+public class FindMin {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length;
+        int min = Integer.MAX_VALUE;
+        while (left < right) {
+            int mid = left + (right - left) / 2;//取中间位置
+            if (nums[mid] == nums[left]) {
+                // 可能是递增的序列
+                min = Math.min(min, nums[left]);
+                left++;
+            } else if (nums[mid] > nums[left]) {
+                // 有序的
+                min = Math.min(min, nums[left]);
+                left = mid + 1;
+            } else {
+                min = Math.min(min, nums[mid]);
+                right = mid;
+            }
+        }
+        if (left != nums.length) {
+            min = Math.min(min, nums[left]);
+        }
+        return min;
+    }
+}
