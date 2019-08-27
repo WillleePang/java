e6be1855758ba436f939607147d1363621e08d96
willleepang <pangwilllee@hotmail.com>
2019-08-27 17:27:48 +0800

    add some solution

---
+++ Advance/src/main/java/com/willlee/leetcode/array/MaxAreaOfIsland.java
@@ -0,0 +1,28 @@
+package com.willlee.leetcode.array;
+
+//leetcode695
+public class MaxAreaOfIsland {
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid.length == 0)
+            return 0;
+        int m = grid.length, n = grid[0].length;
+        int max_area = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    max_area = Math.max(max_area, dfs(grid, i, j));
+                }
+            }
+        }
+        return max_area;
+    }
+
+    public int dfs(int[][] grid, int row, int col) {
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == 0) {
+            return 0;
+        }
+        grid[row][col] = 0;
+        return 1 + dfs(grid, row - 1, col) + dfs(grid, row, col - 1) + dfs(grid, row + 1, col)
+                + dfs(grid, row, col + 1);
+    }
+}
--- Advance/src/main/java/com/willlee/leetcode/array/MaxProfit.java
+++ Advance/src/main/java/com/willlee/leetcode/array/MaxProfit.java
@@ -14,17 +14,77 @@ public class MaxProfit {
         return max;
     }
 
+    public int maxProfit11(int[] prices) {
+        if (prices.length < 1) {
+            return 0;
+        }
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+            } else {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+            }
+        }
+        return dp[n - 1][0];
+    }
+
+    public int maxProfit12(int[] prices) {
+        if (prices.length < 1) {
+            return 0;
+        }
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, -prices[i]);
+        }
+        return dp_i_0;
+    }
+
     // leetcode122 只要有正数差值，就有利润，一直累加，就能获得最大利润
     public static int maxProfit2(int[] prices) {
         if (prices.length <= 1) {
             return 0;
-        } else {
-            int max = 0;
-            for (int i = 0; i < prices.length - 1; i++) {
-                max += prices[i + 1] > prices[i] ? prices[i + 1] - prices[i] : 0;
-            }
-            return max;
         }
+        int max = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            max += prices[i + 1] > prices[i] ? prices[i + 1] - prices[i] : 0;
+        }
+        return max;
+    }
+
+    public static int maxProfit21(int[] prices) {
+        if (prices.length <= 1) {
+            return 0;
+        }
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+            } else {
+                int temp = dp[i - 1][0];
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], temp - prices[i]);
+            }
+        }
+        return dp[n - 1][0];
+    }
+
+    public static int maxProfit22(int[] prices) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
+        }
+        return dp_i_0;
     }
 
     // leetcode123
@@ -55,6 +115,96 @@ public class MaxProfit {
             maxProfit = Math.max(maxProfit, leftMaxProfit[i] + rightMaxProfit[i]);
         }
         return maxProfit;
+    }
+
+    public static int maxProfit31(int[] prices) {
+        int dp_i10 = 0, dp_i11 = Integer.MIN_VALUE;
+        int dp_i20 = 0, dp_i21 = Integer.MIN_VALUE;
+        for (int price : prices) {
+            dp_i20 = Math.max(dp_i20, dp_i21 + price);
+            dp_i21 = Math.max(dp_i21, dp_i10 - price);
+            dp_i10 = Math.max(dp_i10, dp_i11 + price);
+            dp_i11 = Math.max(dp_i11, -price);
+        }
+        return dp_i20;
+    }
+
+    // leetcode714
+    public int maxProfit4(int[] prices, int fee) {
+        int cash = 0, hold = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            cash = Math.max(cash, hold + prices[i] - fee);
+            hold = Math.max(hold, cash - prices[i]);
+        }
+        return cash;
+    }
+
+    public int maxProfit41(int[] prices, int fee) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i] - fee);
+        }
+        return dp_i_0;
+    }
+
+    // leetcode309
+    public int maxProfit5(int[] prices) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        int dp_pre_0 = 0;// 代表dp[i-2][0]
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, dp_pre_0 - prices[i]);
+            dp_pre_0 = temp;
+        }
+        return dp_i_0;
+    }
+
+    // leetcode188
+    public int maxProfit6(int k, int[] prices) {
+        int n = prices.length;
+        if (k > n / 2) {
+            // 注意：可在同一天卖出和买入
+            int[] dp = new int[prices.length + 1];
+            dp[0] = 0;
+            for (int i = 0; i < dp.length - 1; i++) {
+                if (i == 1)
+                    dp[1] = 0;
+                for (int j = i + 1; j < dp.length - 1; j++) {
+                    dp[j + 1] = dp[i + 1];
+                    if (prices[i] >= prices[j])
+                        continue;
+                    if (dp[i + 1] + prices[j] - prices[i] > dp[j + 1])
+                        dp[j + 1] = dp[i + 1] + prices[j] - prices[i];
+                }
+            }
+            return dp[dp.length - 1];
+        }
+        int minprices = prices[0];
+        int[][][] dp = new int[n + 1][k + 1][2];
+        for (int i = 0; i < n; i++) {
+            if (prices[i] < minprices)
+                minprices = prices[i];
+            for (int m = 0; m <= k; m++) {
+                if (m == 0) {
+                    dp[i + 1][m][0] = 0;
+                    dp[i + 1][m][1] = 0 - minprices;
+                    continue;
+                }
+                if (i == 0) {
+                    dp[i + 1][m][0] = 0;
+                    dp[i + 1][m][1] = 0 - prices[0];
+                    continue;
+                }
+                dp[i + 1][m][0] = Math.max(dp[i][m][0], dp[i][m - 1][1] + prices[i]);
+                dp[i + 1][m][1] = Math.max(dp[i][m][1], dp[i][m][0] - prices[i]);
+            }
+        }
+        return dp[n][k][0];
     }
 
     public static void main(String[] args) {
