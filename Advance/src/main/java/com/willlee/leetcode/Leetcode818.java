14e06b5464939408327c54b7ae3863493d775ff6
willleepang <pangwilllee@hotmail.com>
2020-05-08 13:58:42 +0800

    add solutions

---
+++ Advance/src/main/java/com/willlee/leetcode/Leetcode818.java
@@ -0,0 +1,58 @@
+package com.willlee.leetcode;
+
+public class Leetcode818 {
+    public int racecar(int target) {
+        int[] suport = new int[target + 1];
+        int res = dp(target, suport);
+        return res;
+    }
+
+    public int dp(int target, int[] support) {
+        if (target <= 0) {
+            return 0;
+        }
+        if (target == 1) {
+            return 1;
+        }
+        if (target == 2) {
+            return 4;
+        }
+        if (support[target] != 0) {
+            return support[target];
+        }
+
+        int n = 0;
+        // log2(t+1)获得幂数n
+        for (int i = 0; i < target; i++) {
+            if ((1 << i) > target) {
+                n = i;
+                break;
+            }
+        }
+        if (target + 1 == (1 << n)) {
+            return n;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int j = 0; j < n - 1; ++j) {
+            min = Math.min(min, dp((int) target - (1 << (n - 1)) + (1 << j), support) + n + j + 1);
+        }
+        support[target] = Math.min(min, dp((1 << (n)) - (target + 1), support) + n + 1);
+        return support[target];
+    }
+
+    int[] dp = new int[10001];
+
+    public int racecar1(int t) {
+        if (dp[t] > 0)
+            return dp[t];
+        int n = (int) (Math.log(t) / Math.log(2)) + 1;
+        if (1 << n == t + 1)
+            dp[t] = n;
+        else {
+            dp[t] = racecar1((1 << n) - 1 - t) + n + 1;
+            for (int m = 0; m < n - 1; ++m)
+                dp[t] = Math.min(dp[t], racecar1(t - (1 << (n - 1)) + (1 << m)) + n + m + 1);
+        }
+        return dp[t];
+    }
+}
---
+++ Advance/src/main/java/com/willlee/leetcode/Leetcode882.java
@@ -0,0 +1,52 @@
+package com.willlee.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Leetcode882 {
+    public int reachableNodes(int[][] edges, int M, int N) {
+        Map<Integer, Map<Integer, Integer>> m = new HashMap<>();
+        for (int[] edge : edges) {
+            m.computeIfAbsent(edge[0], t -> new HashMap<>()).put(edge[1], edge[2]);
+            m.computeIfAbsent(edge[1], t -> new HashMap<>()).put(edge[0], edge[2]);
+        }
+        // 大顶堆
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        // {M，0} M表示还剩多少步数，0表示顶点
+        pq.add(new int[] { M, 0 });
+        // 表示节点是否访问过
+        Map<Integer, Integer> seen = new HashMap<>();
+        while (!pq.isEmpty()) {
+            // 取出步数最大的顶点
+            int[] top = pq.poll();
+            // step remain
+            int step = top[0];
+            // current node
+            int node = top[1];
+            // 找到还未被访问的节点
+            if (!seen.containsKey(node)) {
+                // 标记节点已经被访问
+                seen.put(node, step);
+                // 找到与其相连的节点
+                if (m.containsKey(node)) {
+                    for (int next : m.get(node).keySet()) {
+                        // 走过的步数是两点之间的节点数量nodeNums加上next节点，即nodeNums+1
+                        int nodeNums = m.get(node).get(next);
+                        int left = step - (nodeNums + 1);
+                        if (left >= 0 && m.containsKey(next)) {
+                            pq.add(new int[] { left, next });
+                        }
+                    }
+                }
+            }
+        }
+        int res = seen.size();
+        for (int[] edge : edges) {
+            int a = seen.getOrDefault(edge[0], 0);
+            int b = seen.getOrDefault(edge[1], 0);
+            res += Math.min(a + b, edge[2]);
+        }
+        return res;
+    }
+}
