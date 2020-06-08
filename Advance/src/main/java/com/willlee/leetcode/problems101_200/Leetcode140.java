4ebecdcf343aa21a182b092cac1c46d19d0bcd49
willlee <pangwilllee@hotmail.com>
2020-06-08 22:38:05 +0800

    add solutions

--- Advance/src/main/java/com/willlee/leetcode/problems101_200/Leetcode140.java
+++ Advance/src/main/java/com/willlee/leetcode/problems101_200/Leetcode140.java
@@ -2,55 +2,48 @@ package com.willlee.leetcode.problems101_200;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Leetcode140 {
-    // BFS 超时
-    public List<String> wordBreak(String s, List<String> wordDict) {
-        Stack<Leetcode140Pair> stack = new Stack<>();
-        Map<Integer, String> ans = new HashMap<>();
-        for (String word : wordDict) {
-            if (word.length() > s.length()) {
-                continue;
-            }
-            if (s.substring(0, word.length()).equals(word)) {
-                stack.push(new Leetcode140Pair(word.length(), word));
-            }
-        }
-        while (!stack.isEmpty()) {
-            Leetcode140Pair pair = stack.pop();
-            int index = pair.index;
-            if (index == s.length()) {
-                ans.put(pair.index, pair.content);
-            }
-            for (String word : wordDict) {
-                if (index + word.length() > s.length()) {
-                    continue;
-                }
-                if (s.substring(index, index + word.length()).equals(word)) {
-                    String content = pair.content + " " + word;
-                    stack.push(new Leetcode140Pair(index + word.length(), content));
-                }
-            }
-        }
-        return new ArrayList<>(ans.values());
-    }
+	public static void main(String[] args) {
+		Leetcode140 a = new Leetcode140();
+		List<String> list = new ArrayList<String>();
+		list.add("cat");
+		list.add("cats");
+		list.add("and");
+		list.add("sand");
+		list.add("a");
+		a.wordBreak("a", list);
+	}
 
-}
-
-class Leetcode140Pair {
-    public int index;
-    public String content;
-
-    public Leetcode140Pair(int index, String content) {
-        this.index = index;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return index + " " + content;
-    }
+	// BFS 超时
+	public List<String> wordBreak(String s, List<String> wordDict) {
+		HashSet<String> set = new HashSet<String>(wordDict);
+		List<String> ans = new ArrayList<String>();
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		Stack<Integer> stack = new Stack<>();
+		stack.push(0);
+		while (!stack.isEmpty()) {
+			int start = stack.pop();
+			for (int end = start + 1; end <= s.length(); end++) {
+				if (set.contains(s.substring(start, end))) {
+					if (map.containsKey(start)) {
+						map.put(end, map.get(start) + " " + s.substring(start, end));
+					} else {
+						map.put(end, s.substring(start, end));
+					}
+					if (end == s.length()) {
+						ans.add(map.get(end));
+					} else {
+						stack.push(end);
+					}
+				}
+			}
+		}
+		return ans;
+	}
 }
